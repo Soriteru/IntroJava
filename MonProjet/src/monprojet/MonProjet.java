64b685f44592bffb29641619f614ba8a9306a89d
Soriteru <sohanblemandpro@gmail.com>
2026-03-24 13:51:31 +0100

    First Commit

--- MonProjet/src/monprojet/MonProjet.java
+++ MonProjet/src/monprojet/MonProjet.java
@@ -14,7 +14,8 @@ public class MonProjet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    System.out.println("Hello World");
+        System.out.println("");
+        System.out.println("monprojet.MonProjet.main()");
     }
     
 }
