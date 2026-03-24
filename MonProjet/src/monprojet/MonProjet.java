866f3705efa9a7a283876c5ba2565383bde09e9e
Soriteru <sohanblemandpro@gmail.com>
2026-03-24 16:01:25 +0100

    Exercice 7

--- MonProjet/src/monprojet/MonProjet.java
+++ MonProjet/src/monprojet/MonProjet.java
@@ -15,8 +15,11 @@ public class MonProjet {
      */
     
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+        int i = 0;
+        
+        while (i <= 10){
+            System.out.println("i est égal à "+i);
+            i++;
         }
     }
      
