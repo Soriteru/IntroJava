ed17423f14559bf5ff5359fb050fd1f1181d3455
Soriteru <sohanblemandpro@gmail.com>
2026-03-24 14:03:49 +0100

    Exercice 1

--- MonProjet/src/monprojet/MonProjet.java
+++ MonProjet/src/monprojet/MonProjet.java
@@ -14,8 +14,35 @@ public class MonProjet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
-        System.out.println("monprojet.MonProjet.main()");
+        System.out.println("Exercice 1");
+        
+        System.out.println("Variable char");
+        char a = '?';
+        System.out.println(a);
+        
+        System.out.println("Variable int");
+        int b = 1;
+        System.out.println(b);
+        
+        System.out.println("Variable long");
+        long c = 99999999;
+        System.out.println(c);
+        
+        System.out.println("Variable float");
+        float d = 6.7F;
+        System.out.println(d);
+        
+        System.out.println("Variable double");
+        double e = 857493.3837394;
+        System.out.println(e);
+        
+        System.out.println("Variable boolean");
+        boolean f = true;
+        System.out.println(f);
+        
+        System.out.println("Variable string");
+        String g = "Soriteru";
+        System.out.println(g);
     }
     
 }
